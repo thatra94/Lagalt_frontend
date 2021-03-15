6467bc3fe15eb672048fb37280bcf5f1f8205973
“Thanh” <thatra94@gmail.com>
2021-03-15 09:37:49 +0100

    Update Main.js

--- client/src/Main/Main.js
+++ client/src/Main/Main.js
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { Navbar } from "../components/Navbar/navbar";
+import { Navbar } from "../components/Navbar/Navbar";
 
 export const Main = () => {
   return (

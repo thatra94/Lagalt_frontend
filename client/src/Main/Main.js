ef2a28a5062ff3c40b66c2d458cacae09b8a13ca
jakob ramstad <jakob.ramstad@outlook.com>
2021-03-16 14:55:31 +0100

    Restructured projectbanner component

--- client/src/Main/Main.js
+++ client/src/Main/Main.js
@@ -1,13 +1,18 @@
 import { Link } from "react-router-dom";
-import { Navbar } from "../components/navbar/Navbar";
-import { ProjectBanners } from "./ProjectsBanners";
-
+import { NavBar } from "./NavBar";
+import {Navbar} from "../components/navbar/Navbar"
+import { ProjectBanners } from "./ProjectBanner/ProjectsBanners";
+import { Container } from '@material-ui/core';
+import {useHistory} from 'react-router-dom';
 export const Main = () => {
+  const history = useHistory();
   return (
-    <div>
-      <Navbar></Navbar>
-     
-      <ProjectBanners projects={[{title: "Project1"},{title: "Project1"},{title: "Project1"}]}/>
+   <div>
+      <NavBar history={history}></NavBar>
+      <Navbar ></Navbar>
+      <Container>
+      <ProjectBanners/>
+    </Container>
     </div>
   );
 };
--- client/src/Main/ProjectsBanners.js
+++
@@ -1,9 +0,0 @@
-import {ProjectBannerItem} from './ProjectBannerItem';
-
-export const ProjectBanners = ({ projects }) => {
-    return (
-        <div>
-            { projects.map(project => <ProjectBannerItem key={project.id} project={project} />)}
-        </div>
-    )
-}

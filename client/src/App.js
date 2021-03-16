b9d5b1e6292cbbcd22f5247f41c12cc1a28ad743
“Thanh” <thatra94@gmail.com>
2021-03-16 17:19:20 +0100

    FEAT: Bugfix posting

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,6 @@ export const App = () => {
       <BrowserRouter>
         <div className="App">
           <Link to="/">Main</Link>
-          <img src={logo} className="App-logo" alt="logo" />
           <Switch>
             <Route exact path="/" component={Main} />
             <PrivateRoute path="/profile" component={Profile} />

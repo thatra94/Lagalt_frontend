7167e787451db9a794839176f188bf1bbc05e8b0
“Thanh” <thatra94@gmail.com>
2021-03-23 18:25:48 +0100

    Update index.js

--- client/src/store/reducers/index.js
+++ client/src/store/reducers/index.js
@@ -3,13 +3,11 @@ import { userReducer } from "./userReducer";
 import { projectsOverviewReducer } from "./projectsOverviewReducer";
 import { projectReducer } from "./projectReducer";
 import { commentReducer } from "./commentReducer";
-
-
 import { userProjectsReducer } from "./userProjectsReducer";
 export const rootReducers = combineReducers({
   userReducer,
   projectsOverviewReducer,
   projectReducer,
-  commentReducer
+  commentReducer,
   userProjectsReducer,
 });

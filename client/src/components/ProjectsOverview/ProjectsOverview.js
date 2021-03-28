5613e4e65b77ada31bcfae09762ea5a547d67f89
“Thanh” <thatra94@gmail.com>
2021-03-28 14:51:58 +0200

    create project

--- client/src/components/ProjectsOverview/ProjectsOverview.js
+++ client/src/components/ProjectsOverview/ProjectsOverview.js
@@ -1,5 +1,5 @@
-import { Navbar } from '../Shared/Navbar/Navbar';
-import { ProjectBanners } from '../Shared/ProjectBanner/ProjectsBanners';
+import { Navbar } from "../Shared/Navbar/Navbar";
+import { ProjectBanners } from "../Shared/ProjectBanner/ProjectsBanners";
 import {
   Container,
   Button,
@@ -8,16 +8,16 @@ import {
   Grid,
   Input,
   makeStyles,
-} from '@material-ui/core';
-import { useHistory } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import { useEffect } from 'react';
-import { projectsOverviewFetchAction } from '../../store/actions/projectsOverviewActions';
-import { ProjectsOverviewSearch} from "./ProjectsOverviewSearch"
-import { ProjectsOverviewIndustries} from "./ProjectsOverviewIndustries"
+  Box,
+} from "@material-ui/core";
+import { useHistory } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { projectsOverviewFetchAction } from "../../store/actions/projectsOverviewActions";
+import { ProjectsOverviewSearch } from "./ProjectsOverviewSearch";
+import { ProjectsOverviewIndustries } from "./ProjectsOverviewIndustries";
 
 export const ProjectsOverview = () => {
-
   const history = useHistory();
 
   const dispatch = useDispatch();
@@ -25,7 +25,7 @@ export const ProjectsOverview = () => {
     (state) => state.projectsOverviewReducer
   );
   const classes = useStyles();
-  
+
   useEffect(() => {
     dispatch(projectsOverviewFetchAction());
   }, []);
@@ -35,19 +35,27 @@ export const ProjectsOverview = () => {
       <Container maxWidth="xl">
         {error && <p>{error}</p>}
         {fetching && <p>Getting projects...</p>}
-        <Container maxWidth="xl"></Container>
+        <Grid container justify="center" alignContent="center" spacing={0}>
+          <Grid item container xs={9} md={9} spacing={2}>
+            <Grid item xs={11} md={11}>
+              <Paper className={classes.paper}>
+                <ProjectsOverviewIndustries />
+              </Paper>
+            </Grid>
+            <Grid item xs={11} md={11} styles={{ marginTop: "1rem" }}>
+              <ProjectBanners projects={projects} />
+            </Grid>
+          </Grid>
 
-        <Grid container spacing={3}>
-          <Grid item xs={12} md={9}>
-            <Paper className={classes.paper}>
-            <ProjectsOverviewIndustries/>
-            </Paper>
-          </Grid>
-          <Grid item xs={12} md={3}>
-            <Paper className={classes.paper}> <ProjectsOverviewSearch/> </Paper>
-          </Grid>
-          <Grid item xs={12} md={9}>
-            <ProjectBanners projects={projects} />
+          <Grid item container direction="column" xs={3} md={3}>
+            <Grid item xs={10}>
+              <Paper className={classes.paper}>
+                <ProjectsOverviewSearch />
+              </Paper>
+              <Paper className={classes.paper}>
+                <Button variant="contained">Opprett et nytt prosjekt</Button>
+              </Paper>
+            </Grid>
           </Grid>
         </Grid>
       </Container>
@@ -56,13 +64,14 @@ export const ProjectsOverview = () => {
 };
 const useStyles = makeStyles((theme) => ({
   pageContentContainer: {
-    textAlign: 'start',
-    maxWidth: 'xl',
+    textAlign: "start",
+    maxWidth: "xl",
     paddingLeft: theme.spacing(8),
     paddingRight: theme.spacing(8),
   },
   paper: {
-    textAlign: 'left',
+    textAlign: "left",
     padding: theme.spacing(2),
+    marginTop: theme.spacing(2),
   },
 }));
--- client/src/components/ProjectsOverview/ProjectsOverviewSearch.js
+++ client/src/components/ProjectsOverview/ProjectsOverviewSearch.js
@@ -1,56 +1,65 @@
-import {Button, TextField ,Container,createStyles, makeStyles, Theme} from '@material-ui/core'
-import {useState} from 'react'
-import {useSelector, useDispatch} from 'react-redux'
-import { projectsOverviewSearchAction } from '../../store/actions/projectsOverviewActions';
+import {
+  Button,
+  TextField,
+  Container,
+  createStyles,
+  makeStyles,
+  Theme,
+  Grid,
+  Paper,
+} from "@material-ui/core";
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { projectsOverviewSearchAction } from "../../store/actions/projectsOverviewActions";
 import React from "react";
 
-
 export const ProjectsOverviewSearch = () => {
+  const [search, setSearch] = useState("");
+  const dispatch = useDispatch();
 
-    const [search, setSearch] = useState('');
-    const dispatch = useDispatch();  
-   
-  
-    const classes = useStyles();
+  const classes = useStyles();
 
-    const handleChange = (event) => {
-        setSearch(event.target.value);
-          console.log(search)
-      }; 
+  const handleChange = (event) => {
+    setSearch(event.target.value);
+    console.log(search);
+  };
 
-      const handleSearch = () => {        
-        dispatch(projectsOverviewSearchAction(search));
-        setSearch('');
-      }; 
+  const handleSearch = () => {
+    dispatch(projectsOverviewSearchAction(search));
+    setSearch("");
+  };
 
-
-    return (<>
-        <Container >
-            
-        <TextField 
-        InputProps={{
-            className: classes.input
-         }}
-        size="small"
-        variant="outlined" 
-        id="outlined-fullwidth-motivasjonstekst"
-        type="text"
-        onChange={handleChange}
-        placeholder="Search for projects…"
-        inputProps={{ 'aria-label': 'search' }}
-        value={search}
+  return (
+    <Grid container justify="center" alignItems="stretch">
+      <Grid item xs={12}>
+        <TextField
+          InputProps={{
+            className: classes.input,
+          }}
+          size="small"
+          variant="outlined"
+          id="outlined-fullwidth-motivasjonstekst"
+          type="text"
+          onChange={handleChange}
+          placeholder="Search for projects…"
+          inputProps={{ "aria-label": "search" }}
+          value={search}
         />
-        <Button variant="outlined" onClick={handleSearch}>
-            Search
-        </Button></Container>
-        </>
-    )
-
-}
+        <Button variant="contained" onClick={handleSearch}>
+          Search
+        </Button>
+      </Grid>
+    </Grid>
+  );
+};
 const useStyles = makeStyles((theme) =>
   createStyles({
     input: {
-      height: 36
-    }
+      height: 36,
+    },
+    paper: {
+      textAlign: "left",
+      padding: theme.spacing(2),
+    },
   })
 );
